277aea588d646dea1c7ef38d014ce6918784b8da
timestorms <764310576@qq.com>
2019-04-11 23:20:55 +0800

    Update mail.inc.php

--- conf/mail.inc.php
+++ conf/mail.inc.php
@@ -1,7 +1,7 @@
 <?php
 	/**
 	 +=========================================================
-	 * 项目名称：卖客疯
+	 
 	 * email.inc.php      邮件配置
 	 +=========================================================
 	 */
@@ -9,7 +9,7 @@
 		'mail_config' => array(
 				'host' => 'smtp.exmail.qq.com',
 				'username' => 'monitor@tencent.com',
-				'password' => 'Monitor20170519??',
+				'password' => '123456',
 				'port' => 25,
 				'from' => 'monitor@tencent.com',
 				'subject' => '邮件中心',
